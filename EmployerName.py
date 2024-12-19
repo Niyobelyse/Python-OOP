16671d4293da696851b8a64bfc4730cb2becc9f1
Niyonsenga-Belyse <b.niyonseng@alustudent.com>
2024-12-19 11:17:49 +0200

    Display method added

--- EmployerName.py
+++ EmployerName.py
@@ -3,3 +3,9 @@ class Employer:
         self.Name = input("Enter your Name")
         self.Department = input("Enter your Department")
         self.Age = input("Enter your Age")
+    
+    def Display(self):
+        print("Your data will be displayed in this format")
+        print(self.Name)
+        print(self.Department)
+        print(self.Age)
