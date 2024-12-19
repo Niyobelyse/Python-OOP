13412d3a5e5225b964ed36fa730979194eadf82d
Niyonsenga-Belyse <b.niyonseng@alustudent.com>
2024-12-19 16:42:12 +0200

    child class added with messaging functionality

--- Telephone.py
+++ Telephone.py
@@ -7,14 +7,25 @@ class Telephone:
     
 
     def Calling(self):
-        self.name = input("Enter name to call")
-        if self.name in self.Phonebook.values():
-            print(f"Calling {self.name}")
+        self.name_to_call = input("Enter name to call")
+        if self.name_to_call in self.Phonebook.values():
+            print(f"Calling {self.name_to_call}")
         else:
-            print(f"We don't have {self.name} in phonebook")
+            print(f"We don't have {self.name_to_call} in phonebook")
 
 
 
-phone1 = Telephone()
+class CellPhone(Telephone):
+    def __init__(self):
+        super().__init__()
+    def Messaging(self):
+        self.name_to_message = input("Enter name to massage")
+        if self.name_to_message in self.Phonebook.values():
+            print(f"Message to {self.name_to_message}")
+        else:
+            print(f"We don't have {self.name_to_message} in phonebook")      
+
+
+phone1=CellPhone()
 phone1.Calling()
-  
+phone1.Messaging()
