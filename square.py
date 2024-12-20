5ff1f085a84e2a89fc88aabe0c34cc102478dde6
Niyonsenga-Belyse <b.niyonseng@alustudent.com>
2024-12-20 18:19:35 +0200

    Access and update private attribute

--- square.py
+++ square.py
@@ -1,11 +1,23 @@
-"""Respresents class of square"""
+#!/usr/bin/python3
+'''Defining the class Square'''
+
+
 class Square():
-    def __init__(self,size = 0):
-        if not isinstance(size,int):
-            raise TypeError("number must be integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+    '''Definition of class here'''
+    def __init__(self, size=0):
         self.__size = size
-    
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
     def area(self):
-        return self.__size * self.__size
+        return (self.__size * self.__size)
