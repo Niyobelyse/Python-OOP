8a37b97b987dc3a8790346c1f84ecaea709e0a8a
Niyonsenga-Belyse <b.niyonseng@alustudent.com>
2024-12-20 16:28:41 +0200

    codes with different performance

--- print_number.py
+++ print_number.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""will not raise an error if number is not an integer."""
 number = 98
+print(f"{number} Battery street")
+
+
+"""will raise an error if number is not an integer."""
+number = "98"
 print(f"{number:d} Battery street")
