6029a9ca15606ae66f43ecf56c1c0ef6a4570fa2
Niyonsenga-Belyse <b.niyonseng@alustudent.com>
2024-12-20 16:29:34 +0200

    comment of error from variable 2

--- print_number.py
+++ print_number.py
@@ -5,5 +5,5 @@ print(f"{number} Battery street")
 
 
 """will raise an error if number is not an integer."""
-number = "98"
+number = "98" #will raise an error because variable number holds string
 print(f"{number:d} Battery street")
