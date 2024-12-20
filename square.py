725c7f6f71abb521c536035057fcfbc1d38b889c
Niyonsenga-Belyse <b.niyonseng@alustudent.com>
2024-12-20 17:35:35 +0200

    added validation

--- square.py
+++ square.py
@@ -1,5 +1,8 @@
 """Respresents class of square"""
 class Square():
-    def __init__(self,size):
+    def __init__(self,size = 0):
+        if not isinstance(size,int):
+            raise TypeError("number must be integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
-        
