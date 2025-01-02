59a0957b49835eaa2e6713e9a4d9e7978cd513f5
Niyonsenga-Belyse <b.niyonseng@alustudent.com>
2025-01-02 17:31:30 +0200

    Name's value will be in lowercase

--- stringvariables.py
+++ stringvariables.py
@@ -1,4 +1,5 @@
-name = "She is beautful"
+name = "She is BEAUTFUL"
 print(name)
 print(name.title())  #each word starts with a capital letter
 print(name.upper()) # Name's value will be in Uppercase
+print(name.lower()) # Name's value will be in lowercase
